41eb2f6256cdc32e2f47fcb7b0f959c205a81959
Andrew English <aje299@gmail.com>
2022-01-04 17:30:17 -0800

    Create WS connection

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { logger, trafficLogger, errorLogger } = require('./lib/logger');
+const { Connection } = require('./lib/connection');
+
+let connection;
 const app = express();
 
 // logging middleware
@@ -11,7 +14,12 @@ app.use(express.json({extended: false}));
 // app.use(express.urlencoded({ extended: true }));
 
 // app routes
-router.get('/health', (_, res) => { res.status(200).json({ message: 'healthy' }) });
+router.get('/health', (_, res) => {
+  if(connection.connected)
+    res.status(200).json({ message: 'healthy' });
+  else
+    res.status(500).json({ message: 'not healthy' });
+});
 app.use(router);
 
 // error logging
@@ -20,4 +28,5 @@ app.use(errorLogger);
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   logger.info(`Server started on port: ${port}`);
+  connection = new Connection();
 });
